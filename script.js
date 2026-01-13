5712a5cbbae68418d1cca370b9ab86954d74cded
Qazi-Ghulam-Mustafa <Qazi-Ghulam-Mustafa>
2026-01-14 01:52:49 +0500

    Complete

--- script.js
+++ script.js
@@ -1,94 +1,69 @@
-var inputData = document.getElementById("input")
-console.log(inputData)
-
-document.addEventListener("keydown", function (val) {
-    console.log(val);
-    val.preventDefault();
-
-    if (val.key == "Enter") {
-        Equal();
-    } else if (Number(val.key) >= 0 && Number(val.key <= 9)) {
-        //  10
-        setNum(val.key);
-    }
-})
-
-
-function setNum(num) {
-    var lastValue = inputData.value.toString().slice(inputData.value.length - 1)
+var inputResult = document.getElementById("inputResult")
+function number(num) {
+    // console.log(num)
+    // inputResult.value += num
+    var lastValue = inputResult.value.toString().slice(inputResult.value.length - 1)
     console.log(lastValue)
-    if (
-        lastValue == "+" ||
-        lastValue == "-" ||
-        lastValue == "*" ||
-        lastValue == "/"
-    ) {
+    if (lastValue == "+" || lastValue == "-" || lastValue == "*" || lastValue == "/") {
         if (num == "+" || num == "-" || num == "*" || num == "/") {
             Toastify({
-                text: "Enter Correct Data",
+                text: "Enter Operator Only one Time",
                 className: "info",
                 style: {
-                    background: "linear-gradient(to right, red, #197216a5, #96c93d)",
+                    background: "linear-gradient(to right, #b00000, #ff0404)",
                 }
             }).showToast();
         }
         else {
-            inputData.value += num;
+            inputResult.value += num
         }
     } else {
-        inputData.value += num;
+        inputResult.value += num
     }
 }
+function equal() {
+    inputResult.value = eval(inputResult.value)
+}
+function clearAll() {
+    inputResult.value = ""
 
-function Equal() {
-    inputData.value = eval(inputData.value)
+}
+function backSpace() {
+    inputResult.value = inputResult.value.toString().slice(0, inputResult.value.length - 1)
 }
 function newSet() {
-    inputData.value = `(-${inputData.value})`
-
+    inputResult.value = `(-${inputResult.value})`
 }
-
-function backSpace() {
-    inputData.value = inputData.value.toString().slice(0, inputData.value.length - 1)
+function square() {
+    inputResult.value = inputResult.value * inputResult.value
 }
-
-function clearAll() {
-    inputData.value = ""
+function cube() {
+    inputResult.value = inputResult.value * inputResult.value * inputResult.value
 }
-function squareData() {
-    inputData.value = inputData.value * inputData.value
+function squareRoot() {
+    inputResult.value = Math.sqrt(inputResult.value)
 }
-
-function cubeData() {
-    inputData.value = inputData.value * inputData.value * inputData.value
+function cubeRoot() {
+    inputResult.value = Math.cbrt(inputResult.value)
 }
-
-function squareRootData() {
-    inputData.value = Math.sqrt(inputData.value)
-}
-
 function percentage() {
-  // 2 of 30%o
-  var value = inputData.value+"%"
-  if (value.toString().includes("%")) {
-    var numb = parseFloat(value); //2+50%=>2+50,  50%=>50
+    var Value = inputResult.value + "%"
+    if (Value.toString().includes("%")) {
+        var numb = parseFloat(Value);
 
-    var result = numb / 100;
+        result = numb / 100; //2+50% => 2+50, 50% => 50
 
-    inputData.value = result;
-  }
-  if (value.toString().includes("%") && value.toString().includes("+")) {
-    var numb = value.toString().split("+"); //  2+50%
-    // 2,50%
+        inputResult.value = result;
+    }
+    if (Value.toString().includes("%") && Value.toString().includes("+")) {
+        var numb = Value.toString().split("+"); //2+50% , //2,50%
 
-    var numb1 = Number(numb[0]); //"2"
-    var numb2 = numb[1]; //50%
+        var num1 = Number(numb[0]);  // 2
+        var num2 = numb[1];  //50%
+        var num3 = parseFloat(num2) / 100;  //50% => 50/100
+        var num4 = parseFloat(num3) * Number(num1) // 0.5*2 => 1    
 
-    var num3 = parseFloat(numb2) / 100; //=> 50%=>50/100
-    var numb4 = parseFloat(num3) * Number(numb1); //0.5 * 2=> 1
-    
-
-    var result = numb4 + numb1; //1+2=>3
-    inputData.value = result;
-  }
+        var result = num4 + num1 // 1+2 => 3
+        inputResult.value = result
+    }
 }
